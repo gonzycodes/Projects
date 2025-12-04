cd90f122b731c6ebd56b9f6143eb7d9fd5c6a229
gonzycodes <nahuel.gonzalezj@hotmail.com>
2025-12-04 14:31:00 +0100

    Built a input function in the input_tools module

--- in-progress/pwd-gen/input_tools.py
+++ in-progress/pwd-gen/input_tools.py
@@ -1,1 +1,15 @@
 #Error management and user input
+
+def menu_input() -> int:
+    while True:
+        answer = input('Choose an alternative: ')
+        try:
+            num_answer = int(answer)
+        except:
+         print('You have to write a number')
+         continue
+        if num_answer != 1 and num_answer != 2:
+            print('You have to choose 1 or 2')
+            continue
+        else:
+            return num_answer
