f96639698b2d9744ffb5196af592e1a8fa9b7e85
gonzycodes <nahuel.gonzalezj@hotmail.com>
2025-12-05 17:04:14 +0100

    Started and completed the generate_password function inside the password_tools module

--- in-progress/pwd-gen/password_tools.py
+++ in-progress/pwd-gen/password_tools.py
@@ -1,1 +1,6 @@
-#Everything thats associated with passwords
+import random
+pool= "abcdefghijklmnopqrstuvwxyz"
+pool_numbers = "0123456789"
+
+def generate_password(lenght, include_numbers) -> str:
+    
