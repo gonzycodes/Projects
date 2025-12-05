dc8cb2a25003613f70937a67221c3a206ec8ff11
gonzycodes <nahuel.gonzalezj@hotmail.com>
2025-12-05 15:32:00 +0100

    Completed a lenght function and a password conditions function

--- in-progress/pwd-gen/input_tools.py
+++ in-progress/pwd-gen/input_tools.py
@@ -13,3 +13,35 @@ def menu_input() -> int:
             continue
         else:
             return num_answer
+        
+def lenght() -> int:
+    while True:
+        pwd_lenght = input('How long do you want your password to be?: ')
+        try:
+            num_lenght = int(pwd_lenght)
+        except:
+            print('You have to write an integer.')
+            continue
+        if num_lenght > 20:
+            print('Value cannot be more than 20')
+            continue
+        elif num_lenght > 0:
+            return num_lenght
+        else:
+            print('Value cannot be negative, try again.')
+            continue
+        
+def pwd_conditions():
+    Yes = 'Yy'
+    No = 'Nn'
+    while True:
+        condition = input('Do you want numbers in your password? (Y/N): ')
+        if condition[0] in Yes:
+            condition = True
+            return condition
+        elif condition[0] in No:
+            condition = False
+            return condition
+        else:
+            print('You have to type either Y or N!')
+            continue
