cf42b1947f9a1361c657d13513fd6d5f5e23c897
gonzycodes <nahuel.gonzalezj@hotmail.com>
2025-12-05 17:07:27 +0100

    Now everthing is commited

--- in-progress/pwd-gen/password_tools.py
+++ in-progress/pwd-gen/password_tools.py
@@ -3,4 +3,14 @@ pool= "abcdefghijklmnopqrstuvwxyz"
 pool_numbers = "0123456789"
 
 def generate_password(lenght, include_numbers) -> str:
+    password = ""
+    if include_numbers == True:
+        pool_with_numbers = pool + pool_numbers
+        for x in range(lenght):
+            password = password + random.choice(pool_with_numbers)
+        return password
+    elif include_numbers == False:
+        for x in range(lenght):
+            password = password + random.choice(pool)
+        return password
     
