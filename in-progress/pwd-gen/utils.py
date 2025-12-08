43e36b5fdc36da2999def5022fe6fcd2b7024368
gonzycodes <nahuel.gonzalezj@hotmail.com>
2025-12-08 12:42:26 +0100

    Finished the utils.py module, created functions that make the program look nicer

--- in-progress/pwd-gen/utils.py
+++ in-progress/pwd-gen/utils.py
@@ -1,1 +1,10 @@
-#Basic help utils
+def pwd_title():
+    text = 'Password Generator'
+    title = text.title()
+    return title
+
+def title_separator():
+    print('--------------------')
+    
+def goodbye():
+    print('Thanks for using the password generator!')
